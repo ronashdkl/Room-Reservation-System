ec5a2c6bb863d5b6578fb9831b156402e47f28f3
Ronash Dhakal <ronashmail@gmail.com>
2017-05-10 13:04:26 +0545

    customer model

---
+++ models/Customer.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "customer".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $surname
+ * @property string $phone_number
+ *
+ * @property Reservation[] $reservations
+ */
+class Customer extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'customer';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'surname', 'phone_number'], 'required'],
+            [['name', 'surname'], 'string', 'max' => 50],
+            [['phone_number'], 'string', 'max' => 25],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'surname' => 'Surname',
+            'phone_number' => 'Phone Number',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getReservations()
+    {
+        return $this->hasMany(Reservation::className(), ['customer_id' => 'id']);
+    }
+}
