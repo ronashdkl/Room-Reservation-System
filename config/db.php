f4661877c9c6a48cfa792dc21921ff1f1866ee01
Ronash Dhakal <ronashmail@gmail.com>
2017-05-10 11:04:21 +0545

    setting up db

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=newsReader',
+    'dsn' => 'mysql:host=localhost;dbname=reservation',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
