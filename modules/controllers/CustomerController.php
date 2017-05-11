ed03ce39e0e70700e047c18eccf9d87805e68c41
Ronash Dhakal <ronashmail@gmail.com>
2017-05-11 15:04:39 +0545

    customerController

--- modules/controllers/CustomerController.php
+++ modules/controllers/CustomerController.php
@@ -10,7 +10,7 @@ use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 use \yii\web\Response;
 use yii\helpers\Html;
-
+use yii\filters\AccessControl;
 /**
  * CustomerController implements the CRUD actions for Customer model.
  */
@@ -22,6 +22,17 @@ class CustomerController extends Controller
     public function behaviors()
     {
         return [
+             'access' => [
+             'class' => AccessControl::className(),
+              
+                'rules' => [
+                    [
+                       
+                        'allow' => true,
+                        'roles' => ['Admin'],
+                    ],
+                ],
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
