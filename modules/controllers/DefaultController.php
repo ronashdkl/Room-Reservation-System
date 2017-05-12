ea7b51d64f123b8a48a29899e2125a71f63f64a8
Ronash Dhakal <ronashmail@gmail.com>
2017-05-12 14:36:33 +0545

    default COntroller

--- modules/controllers/DefaultController.php
+++ modules/controllers/DefaultController.php
@@ -3,12 +3,31 @@
 namespace app\modules\controllers;
 
 use yii\web\Controller;
-
+use yii\filters\AccessControl;
 /**
  * Default controller for the `modules` module
  */
 class DefaultController extends Controller
 {
+    
+    public function behaviors() {
+        
+        return [
+            'access' => [
+             'class' => AccessControl::className(),
+                'only' => ['index'],
+                'rules' => [
+                    [
+                        'actions' => ['index'],
+                        'allow' => true,
+                        'roles' => ['Admin'],
+                    ],
+                ],
+            ],
+        ];
+        
+    }
+    
     /**
      * Renders the index view for the module
      * @return string
